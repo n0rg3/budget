af9516a68be21b6e64b1b44c1d7eb54fb9fc30fb
Артур Баширов <a.bashirov@playrix.com>
2025-06-25 19:45:15 +0200

    webapp

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/budget/', // ⚠️ имя репозитория с косыми чертами
+  base: '/budget/',
   plugins: [react()],
 })
