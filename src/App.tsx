6b5e74acd2687c7de0580fa35f41585ed7aa1434
Артур Баширов <a.bashirov@playrix.com>
2025-06-25 09:28:55 +0200

    check

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
+import CategoriesView from "./pages/CategoriesView";
+
 export default function App() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1 style={{ color: 'red' }}>Тестовая надпись — всё работает</h1>
-    </div>
-  );
+  return <CategoriesView />;
 }
