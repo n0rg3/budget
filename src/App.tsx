20f842b6279d20e7cf423ebadaccdfdcf2c3100e
Артур Баширов <a.bashirov@playrix.com>
2025-06-25 13:06:44 +0200

    icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,36 @@
 // src/App.tsx
-import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
-import CategoriesView from "./pages/CategoryView";
+import { BrowserRouter as Router, Routes, Route, NavLink, Navigate } from "react-router-dom";
+import CategoriesView from "./pages/CategoriesView";
 import PieChartView from "./pages/PieChartView";
 import ExpensesListView from "./pages/ExpensesListView";
 
-import "./App.css"; // создадим стили для табов
+import { FaListUl, FaChartPie, FaTags } from "react-icons/fa"; // иконки
+
+import "./App.css";
 
 export default function App() {
   return (
     <Router>
       <div className="app-container">
         <Routes>
-          <Route path="/" element={<CategoriesView />} />
+          <Route path="/" element={<Navigate to="/categories" replace />} />
+          <Route path="/categories" element={<CategoriesView />} />
           <Route path="/chart" element={<PieChartView />} />
           <Route path="/expenses" element={<ExpensesListView />} />
         </Routes>
 
         <nav className="bottom-nav">
-          <NavLink to="/" end>
-            Категории
+          <NavLink to="/categories">
+            <FaTags />
+            <span>Категории</span>
           </NavLink>
           <NavLink to="/chart">
-            Диаграмма
+            <FaChartPie />
+            <span>Диаграмма</span>
           </NavLink>
           <NavLink to="/expenses">
-            Покупки
+            <FaListUl />
+            <span>Покупки</span>
           </NavLink>
         </nav>
       </div>
