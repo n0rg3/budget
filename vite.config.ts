b7834316a50de7365bf1409dbb6603941aecf978
Артур Баширов <a.bashirov@playrix.com>
2025-06-25 23:37:28 +0200

    working

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/budget/', // это важно для GitHub Pages
+  base: '/budget/', // важно для GitHub Pages
   plugins: [react()],
-})
+});
