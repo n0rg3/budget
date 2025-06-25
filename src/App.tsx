76fdee01f182f5486c551751da8553f10301809c
Артур Баширов <a.bashirov@playrix.com>
2025-06-25 09:06:29 +0200

    delete

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-import CategoriesView from "./pages/CategoriesView";
-
-export default function App() {
-  return <CategoriesView />;
-}
--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/budget/', // ⚠️ обязательно с косыми чертами
-  plugins: [react()],
-})
